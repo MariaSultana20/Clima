15fcddf323a445e98a3046cbcc79c90f762cfe06
MariaSultana20 <mariarifa2018@gmail.com>
2026-07-07 16:28:03 +0600

    Perform request using urlSession

--- Clima/WeatherManger.swift
+++ Clima/WeatherManger.swift
@@ -13,6 +13,33 @@ struct WeatherManger {
     
     func fetchWeather(forCity city: String) {
         let url = "\(weatherUrl)&q=\(city)"
-        print(url)
+        performRequest(urlString: url)
+    }
+    
+    func performRequest(urlString: String) {
+        //Create URL
+        guard let url = URL(string: urlString) else {
+            print("Could not create URL")
+            return
+        }
+        //Create a URLSession
+        let urlSession = URLSession(configuration: .default)
+        
+        //Give a task to URLSession
+        let task = urlSession.dataTask(with: url, completionHandler: handle(data:response:error:))
+        
+        //start the task
+        task.resume()
+    }
+    
+    func handle(data: Data?, response: URLResponse?, error: Error?) {
+        if let error {
+            print("Found error \(error)")
+            return
+        }
+        if let data {
+            let dataString = String(data: data, encoding: .utf8)
+            print(dataString)
+        }
     }
 }
